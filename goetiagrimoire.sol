d1bd5923e04cae26cfb3dcdd9d6d56225254d3dc
normancomics.eth <normancomics.eth@ethermail.io>
2026-03-24 16:36:48 -0400

    Create goetiagrimoire.sol
    git commit -m "rituale: GoetiaGrimoire in catena invocatum  Vigiles ligati. Sapientes excitati. Sigilla insculpta. Prima invocatio grimoirii antediluviani completa est."

---
+++ goetiagrimoire.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title GoetiaGrimoire
+ * @author normancomics.eth 2026 A.D.
+ * @notice On-chain grimoire of the Watchers and forbidden knowledge
+ */
+contract GoetiaGrimoire is Ownable {
+
+    // ----------------------
+    // Watchers (Fallen Angels)
+    // ----------------------
+    struct Watcher {
+        string name;
+        string domainKnowledge;
+        bool invoked;
+    }
+    mapping(uint8 => Watcher) public watchers;
+
+    // ----------------------
+    // Sages & Families
+    // ----------------------
+    struct Sage {
+        string name;
+        uint8 familyId;
+        bool activated;
+    }
+    mapping(uint8 => Sage) public sages;      // 0-6: 7 sages
+    mapping(uint8 => string) public families; // 0-12: 13 families
+
+    // ----------------------
+    // Stellar & Temporal Sigils
+    // ----------------------
+    struct Sigil {
+        bytes32 encodedRite;
+        uint256 activationBlock;
+        bool used;
+    }
+    mapping(bytes32 => Sigil) public sigils;
+
+    // ----------------------
+    // Ritual Events
+    // ----------------------
+    event RitualExecuted(string ritualName, address executor, uint256 blockNumber);
+
+    // ----------------------
+    // Constructor - Initialize the Grimoire
+    // ----------------------
+    constructor() Ownable(msg.sender) {
+        // Watchers (Fallen Angels from Book of Enoch / Goetia)
+        watchers[0] = Watcher("Azazel", "Weaponry & metallurgy", false);
+        watchers[1] = Watcher("Semyaza", "Divination & ritual", false);
+        watchers[2] = Watcher("Armaros", "Enochian cryptography", false);
+        watchers[3] = Watcher("Baraqel", "Alchemy & medicine", false);
+        watchers[4] = Watcher("Kokabiel", "Stellar mechanics", false);
+        watchers[5] = Watcher("Tamiel", "Astronomy & time", false);
+        watchers[6] = Watcher("Ramiel", "Secrets of death & resurrection", false);
+        watchers[7] = Watcher("Daniel", "Forbidden arts", false);
+
+        // Sages
+        sages[0] = Sage("Quetzalcoatl", 0, false);
+        sages[1] = Sage("Hermes", 1, false);
+        sages[2] = Sage("Thoth", 2, false);
+        sages[3] = Sage("Oannes", 3, false);
+        sages[4] = Sage("Enki", 4, false);
+        sages[5] = Sage("Hesiod", 5, false);
+        sages[6] = Sage("Ziusudra", 6, false);
+
+        // Families / Bloodlines
+        families[0] = "House of Azazel";
+        families[1] = "House of Semyaza";
+        families[2] = "House of Armaros";
+        families[3] = "House of Baraqel";
+        families[4] = "House of Kokabiel";
+        families[5] = "House of Tamiel";
+        families[6] = "House of Ramiel";
+        families[7] = "House of Daniel";
+        families[8] = "House of Enoch";
+        families[9] = "House of Cain";
+        families[10] = "House of Seth";
+        families[11] = "House of Lamech";
+        families[12] = "House of Methuselah";
+    }
+
+    // ----------------------
+    // Invoke a Watcher
+    // ----------------------
+    function invokeWatcher(uint8 watcherId) public {
+        Watcher storage w = watchers[watcherId];
+        require(!w.invoked, "Watcher already invoked");
+        w.invoked = true;
+
+        emit RitualExecuted(string(abi.encodePacked("WatcherInvocation-", w.name)), msg.sender, block.number);
+    }
+
+    // ----------------------
+    // Activate a Sage with Sigil
+    // ----------------------
+    function activateSage(uint8 sageId, bytes32 sigilHash) public {
+        Sage storage s = sages[sageId];
+        require(!s.activated, "Sage already activated");
+
+        Sigil storage sig = sigils[sigilHash];
+        require(!sig.used, "Sigil already consumed");
+        require(block.number >= sig.activationBlock, "Temporal sigil not yet active");
+
+        s.activated = true;
+        sig.used = true;
+
+        emit RitualExecuted(string(abi.encodePacked("SageActivation-", s.name)), msg.sender, block.number);
+    }
+
+    // ----------------------
+    // Flash Summon Ritual
+    // ----------------------
+    function flashSummon(address token, uint256 amount, bytes memory encodedSteps) public {
+        (bool success,) = token.delegatecall(encodedSteps);
+        require(success, "Ritual failed");
+        emit RitualExecuted("FlashSummon", msg.sender, block.number);
+    }
+
+    // ----------------------
+    // Cross-Chain Portal Invocation
+    // ----------------------
+    function portalInvoke(address target, bytes memory portalRite) public {
+        (bool success,) = target.call(portalRite);
+        require(success, "Portal rite failed");
+        emit RitualExecuted("PortalInvocation", msg.sender, block.number);
+    }
+
+    // ----------------------
+    // Veiled / ZK-style Proof Rite
+    // ----------------------
+    function veiledProof(bytes memory zkRite) public {
+        assembly {
+            let ptr := add(zkRite, 32);
+            let len := mload(zkRite);
+            pop(ptr);
+            pop(len);
+        }
+        emit RitualExecuted("VeiledProof", msg.sender, block.number);
+    }
+
+    // ----------------------
+    // Admin: Register a new Sigil (Owner only)
+    // ----------------------
+    function registerSigil(bytes32 sigilHash, uint256 activationBlock) external onlyOwner {
+        require(sigils[sigilHash].activationBlock == 0, "Sigil already registered");
+        sigils[sigilHash] = Sigil(sigilHash, activationBlock, false);
+    }
+}
